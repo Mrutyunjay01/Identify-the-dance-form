235fcfd9aedce313be6197e4676a4cc0c6b4f85e
Mrutyunjay01 <mrutyunjay.tutulu1021@gmail.com>
2020-06-11 17:43:26 +0530

    Second trial to be made.

---
+++ Utils/data_loader.py
@@ -0,0 +1,12 @@
+import os
+import pandas as pd
+
+traindf = pd.read_csv('../dataset/train.csv')
+testdf = pd.read_csv('../dataset/test.csv')
+
+
+def image_loder(folderPath):
+
+    images = []
+
+    return images
