d63e679dba252f51975ef8fe1b772acab40d06f9
Mrutyunjay01 <mrutyunjay.tutulu1021@gmail.com>
2020-06-12 19:16:20 +0530

    Image loading, dataframe sorting done.

--- Utils/data_loader.py
+++ Utils/data_loader.py
@@ -1,12 +1,55 @@
 import os
+import cv2
 import pandas as pd
+from matplotlib import pyplot as plt
+import argparse
 
+ap = argparse.ArgumentParser()
+ap.add_argument('-d', '--directory', required=True,
+                help='Path to image directory')
+args = vars(ap.parse_args())
+
+'''
 traindf = pd.read_csv('../dataset/train.csv')
 testdf = pd.read_csv('../dataset/test.csv')
+'''
 
 
-def image_loder(folderPath):
+def sort_filanames(dataset):
+    """
 
+    :param dataset: dataframe containing filenames and target values
+    :return:
+    """
+
+    # concatenate the '.jpg' part of the filenames to sort it
+    dataset['Image'] = dataset['Image'].apply(lambda x: x[: x.find('.')])
+
+    # print(dataset.head())
+    dataset = dataset.sort_values('Image')
+
+    dataset = dataset.reset_index(drop=True)
+
+    return dataset
+    pass
+
+
+def image_loder(folderPath, dataset):
     images = []
 
+    for i, filename in enumerate(os.listdir(folderPath)):
+
+        assert filename == dataset.Image[i] + '.jpg'
+        img = cv2.imread(os.path.join(folderPath, filename))
+
+        if img is not None:
+            images.append(img)
     return images
+
+
+if __name__ == '__main__':
+    traindf = pd.read_csv(r'C:\Users\MRUTYUNJAY BISWAL\Desktop\Hackerearth deep learning challenge\dataset\train.csv')
+
+    trainSort = sort_filanames(traindf)
+    trainImages = image_loder(args['directory'], trainSort)
+    print(trainImages)
