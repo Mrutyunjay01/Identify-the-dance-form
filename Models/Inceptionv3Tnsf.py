a8dd16f6472c5c9c8756ac6fa629c8b6514bc91e
Mrutyunjay01 <mrutyunjay.tutulu1021@gmail.com>
2020-06-17 16:39:08 +0530

    fine tunned last 50 layers

--- Models/Inceptionv3Tnsf.py
+++ Models/Inceptionv3Tnsf.py
@@ -1,65 +1,37 @@
 import tensorflow as tf
-from tensorflow.keras.layers import Flatten, Dense, Dropout, Conv2D, BatchNormalization
+from tensorflow.keras.layers import Flatten, Dense, Dropout, Conv2D, BatchNormalization, GlobalAveragePooling2D
 
 
 def model():
     # define the basemodel
-    base_model = tf.keras.applications.InceptionV3(include_top=False,
-                                                   weights='imagenet',
-                                                   input_shape=(440, 440, 3))
-    for layers in base_model.layers:
-        layers.trainable = False
+    basemodel = tf.keras.applications.InceptionV3(include_top=False,
+                                                  input_shape=(440, 440, 3),
+                                                  weights='imagenet')
 
-    # print(base_model.get_layer('conv2d_80').output_shape)
+    # add global pooling
+    new_output = GlobalAveragePooling2D()(basemodel.output)
 
-    x = Conv2D(filters=128, activation='relu', kernel_size=1)(base_model.get_layer('conv2d_80').output)
-    x = BatchNormalization()(x)
+    # add dense layer for classification
+    new_output = Dense(units=8, activation='softmax')(new_output)
 
-    x = Conv2D(filters=256, activation='relu',
-               kernel_size=3)(x)
-    x = BatchNormalization()(x)
+    MoDel = tf.keras.Model(basemodel.inputs, new_output)
 
-    x = Conv2D(filters=256, activation='relu',
-               kernel_size=1)(x)
-    x = BatchNormalization()(x)
+    # set all layers trainable by default
+    for layer in MoDel.layers:
+        layer.trainable = True
 
-    x = Conv2D(filters=512, activation='relu',
-               kernel_size=3)(x)
-    x = BatchNormalization()(x)
+        if isinstance(layer, tf.keras.layers.BatchNormalization):
+            layer.momentum = 0.9
 
-    x = Conv2D(filters=1024, activation='relu',
-               kernel_size=3)(x)
-    x = BatchNormalization()(x)
+    # fix deep layers
+    for layer in MoDel.layers[:-50]:
+        if not isinstance(layer, tf.keras.layers.BatchNormalization):
+            layer.trainable = False
 
-    x = Conv2D(filters=1024, activation='relu',
-               kernel_size=3, strides=2)(x)
-    x = BatchNormalization()(x)
-
-    x = Conv2D(filters=2048, activation='relu',
-               kernel_size=1)(x)
-    x = BatchNormalization()(x)
-
-    x = Flatten()(x)
-
-    x = Dropout(0.4)(x)
-
-    x = Dense(512, activation=tf.nn.leaky_relu)(x)
-
-    x = Dropout(0.4)(x)
-
-    x = Dense(256, activation=tf.nn.leaky_relu)(x)
-
-    x = Dropout(0.4)(x)
-    x = Dense(128, activation=tf.nn.leaky_relu)(x)
-
-    x = Dropout(0.4)(x)
-
-    x = Dense(8, activation=tf.nn.softmax)(x)
-
-    MoDel = tf.keras.Model(base_model.input, x)
-
-    MoDel.compile(loss='categorical_crossentropy',
-                  optimizer='adam',
-                  metrics=['accuracy'])
+    MoDel.compile(
+        loss='categorical_crossentropy',
+        optimizer=tf.keras.optimizers.Adamax(lr=1e-2),
+        metrics=['accuracy']
+    )
 
     return MoDel
